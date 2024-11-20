dee211f68f1891c7ea91a9285c7d046d2312392b
Edmund <edmundgachanja@gmail.com>
2024-11-20 19:47:43 +0300

    Creates 3 functions with loops in index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+let writeCards = ( array, event ) => {
+    let count = 0;
+    let newArray = []
+    for (var i = 0; i < array.length; i++ ){
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+        count += 1;
+    };
+    return newArray;
+
+};
+
+let countDown = (number) => {
+    while ( number >= 0 ) {
+        console.log(number);
+        number -= 1;
+        
+    }
+};
+countDown(10)
+
+
